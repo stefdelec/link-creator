a6c6b1319bfb53d68c520807eea0aa343f2d66d0
stefdelec <stephane@arianee.org>
2019-12-20 11:18:35 +0100

    feat: add links

--- src/App.js
+++ src/App.js
@@ -6,8 +6,21 @@ export default function App() {
     const link=searchParams.get('a');
 
     return (
-        <h1>
-            <a href={link}>LINK {link}</a>
-        </h1>
+        <div>
+
+       {link && <h1>
+           <a href={link}>LINK {link}</a>
+       </h1>}
+
+            <h1>
+                <a href=" https://arian.ee/12425,76!Pprg">LINK  https://arian.ee/12425,76!Pprg</a>
+            </h1>
+
+            <h1>
+                <a href='https://test.arian.ee/12312,xx3crfrcew4z'>LINK https://test.arian.ee/12312,xx3crfrcew4z</a>
+            </h1>
+
+        </div>
+
     );
 }
